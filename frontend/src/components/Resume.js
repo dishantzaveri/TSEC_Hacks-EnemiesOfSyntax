3fa6ff2545bafcd2fa0021b7a3b94f90472f0fbb
talkshrey <talkshrey@gmail.com>
2023-02-03 06:29:35 +0530

    resume changed

--- frontend/src/components/Resume.js
+++ frontend/src/components/Resume.js
@@ -2,6 +2,8 @@ import axios from "axios";
 import { useSelector } from "react-redux";
 import { useState } from "react";
 import { useEffect } from "react";
+import Header from "./Header/Header";
+import CssBaseline from "@mui/material/node/CssBaseline";
 
 export default function Resume() {
   const { token } = useSelector((state) => state.auth);
@@ -64,45 +66,46 @@ export default function Resume() {
   }, []);
 
   return (
-    <div className="p-6 bg-white rounded-lg shadow-md">
-      <h1 className="text-3xl font-medium">{data.User.name}</h1>
-      <div className="my-4">
-        <p className="text-gray-700">{data.User.email}</p>
+    <div>
+      <Header />
+      <div className="p-6 bg-white w-1/3 my-2 mx-28 rounded-lg shadow-md flex flex-col justify-center">
+        <h1 className="text-3xl font-medium">{data.User.name}</h1>
+        <div className="my-4">
+          <p className="text-gray-700">{data.User.email}</p>
+        </div>
+        <h2 className="text-xl font-medium mt-8">Experiences</h2>
+        <ul className="list-disc pl-5">
+          {data.Experience.map((experience) => (
+            <li key={experience.id} className="my-4">
+              <h3 className="text-lg font-medium">Title: {experience.job_title}</h3>
+              <p className="text-gray-700">Company Name: {experience.company_name}</p>
+              <p className="text-gray-700">Industry:
+                {experience.location} &middot; {experience.industry}
+              </p>
+              <p className="text-gray-700">
+                {experience.start_date} - {experience.end_date}
+              </p>
+              <p className="text-gray-700">Description: {experience.description}</p>
+            </li>
+          ))}
+        </ul>
+        <h2 className="text-xl font-medium mt-8">Education</h2>
+        <ul className="list-disc pl-5">
+          {data.Education.map((school) => (
+            <li key={school.id} className="my-4">
+              <h3 className="text-lg font-medium">Institute: {school.institute}</h3>
+              <p className="text-gray-700">Degree: {school.degree}</p>
+              <p className="text-gray-700">Study field: {school.study_field}</p>
+              <p className="text-gray-700">
+                {school.start_date} - {school.end_date}
+              </p>
+              <p className="text-gray-700">Grade: {school.grade}</p>
+              <p className="text-gray-700">Extra Curriculars: {school.extracurriculars}</p>
+              <p className="text-gray-700">Description: {school.description}</p>
+            </li>
+          ))}
+        </ul>
       </div>
-      <h2 className="text-xl font-medium mt-8">Skills</h2>
-
-      <h2 className="text-xl font-medium mt-8">Experiences</h2>
-      <ul>
-        {data.Experience.map((experience) => (
-          <li key={experience.id} className="my-4">
-            <h3 className="text-lg font-medium">{experience.id}</h3>
-            <p className="text-gray-700">{experience.job_title}</p>
-            <p className="text-gray-700">{experience.company_name}</p>
-            <p className="text-gray-700">{experience.location}</p>
-            <p className="text-gray-700">{experience.industry}</p>
-            <p className="text-gray-700">{experience.start_date}</p>
-            <p className="text-gray-700">{experience.end_date}</p>
-            <p className="text-gray-700">{experience.description}</p>
-          </li>
-        ))}
-      </ul>
-
-      <h2 className="text-xl font-medium mt-8">Education</h2>
-      <ul>
-        {data.Education.map((school) => (
-          <li key={school.id} className="my-4">
-            <h3 className="text-lg font-medium">{school.id}</h3>
-            <p className="text-gray-700">{school.institute}</p>
-            <p className="text-gray-700">{school.degree}</p>
-            <p className="text-gray-700">{school.study_field}</p>
-            <p className="text-gray-700">{school.start_date}</p>
-            <p className="text-gray-700">{school.end_date}</p>
-            <p className="text-gray-700">{school.grade}</p>
-            <p className="text-gray-700">{school.extracurriculars}</p>
-            <p className="text-gray-700">{school.description}</p>
-          </li>
-        ))}
-      </ul>
     </div>
   );
 }
